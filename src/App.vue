4c7c174c9d17ea6a96be7881a281a2d4ec7a5cec
hzq <13402318457@163.com>
2019-07-30 11:32:05 +0800

    开始进行项目配置了

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
     <div id="app">
         <div id="nav">
-            laaaaaaaaaaaaaaa
             <router-link to="/">Home</router-link> |
             <router-link to="/about">About</router-link>
         </div>
